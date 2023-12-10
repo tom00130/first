9a895e3d4fb7d6a259b252b4c6014ca4d5c203e2
tom00130 <94624128+tom00130@users.noreply.github.com>
2023-12-11 00:36:57 +0900

    Update slick.js

--- js/slick.js
+++ js/slick.js
@@ -4,15 +4,15 @@ function slideSwitch() {
   if ( $active.length == 0 ) $active = $('#slideshow img:last');
 
   var $next =  $active.next().length ? $active.next()
-     : $('#slideshow img:first');
+    : $('#slideshow img:first');
 
   $active.addClass('last-active');
 
   $next.css({opacity: 0.0})
-     .addClass('active')
-     .animate({opacity: 1.0}, 1000, function() {
+    .addClass('active')
+    .animate({opacity: 1.0}, 1000, function() {
           $active.removeClass('active last-active');
-     });
+    });
 }
 
 $(function() {
