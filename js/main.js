2866d79a20733d5021169d4544a8967b0f21e39b
tom00130 <94624128+tom00130@users.noreply.github.com>
2024-01-03 21:56:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
-$(function () {
-
-  $('a[href^="#"]').click(function () {
-    let href = $(this).attr("href");
-    let target = $(href == "#" || href == "" ? "html" : href);
-    let position = target.offset().top;
-    $("html, body").animate({ scrollTop: position }, 600, "swing");
-
-    return false; 
-  });
-});
-
-
-$('.faq-title').on('click', function () {
-  $('.faq-text').slideUp(500);
-    
-  var findElm = $(this).next(".faq-text");
-    
-  if ($(this).hasClass('close')) {
-    $(this).removeClass('close');
-  } else {
-    $('.close').removeClass('close');
-    $(this).addClass('close');
-    $(findElm).slideDown(500);
-	}
-});
 
 
 $(".toggle_btn").on("click", function () {
-    if ($("header").hasClass("open")) {
-      $("header").removeClass("open");
-    } else {
-      $("header").addClass("open");
-    }
-  });
+  if ($("header").hasClass("open")) {
+    $("header").removeClass("open");
+  } else {
+    $("header").addClass("open");
+  }
+});
 
-  $('.mask').on('click', function () {
-
+$('.nav-link').on('click', function () {
     $('header').removeClass('open');
   });
-
+  
+  $(function () {
+    
+    $('a[href^="#"]').click(function () {
+      let href = $(this).attr("href");
+      let target = $(href == "#" || href == "" ? "html" : href);
+      let position = target.offset().top - 100;
+      $("html, body").animate({ scrollTop: position}, 600, "swing");
+      
+      return false; 
+    });
+  });
+  
+  
+  $('.faq-title').on('click', function () {
+    $('.faq-text').slideUp(500);
+    
+    var findElm = $(this).next(".faq-text");
+    
+    if ($(this).hasClass('close')) {
+      $(this).removeClass('close');
+    } else {
+      $('.close').removeClass('close');
+      $(this).addClass('close');
+      $(findElm).slideDown(500);
+    }
+  });
