bfa6649ecd7821f6b6ed3d41ac06b7da5cb37af7
tom00130 <94624128+tom00130@users.noreply.github.com>
2024-01-03 16:54:59 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,3 @@
-
-
-
-var bar = new ProgressBar.Line(splash_text, {
-  easing: 'easeInOut',
-  duration: 3000,
-  strokeWidth: 0.2,
-  color: '#212121',
-  trailWidth: 0.2,
-  trailColor: '#757575',
-  text: {			
-    style: {
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      padding: '0',
-      margin: '-30px 0 0 0',
-      transform: 'translate(-50%,-50%)',
-      'font-size': '1rem',
-      color: '#212121',
-    },
-    autoStyleContainer: false
-  },
-  step: function (state, bar) {
-    bar.setText(Math.round(bar.value() * 100) + ' %');
-  }
-});
-
-bar.animate(1.0, function () {
-});
-
-
-
-var stroke;
-stroke = new Vivus('mask', {
-  start: 'manual',
-  type: 'scenario-sync',
-  duration: 20,
-  forceRender: false,
-  animTimingFunction: Vivus.EASE,  
-},
-function () {
-    $("#mask").attr("class", "done");
-  }
-  );
-  
-  $(window).on('load', function () {
-    $("#splash").delay(4000).fadeOut('slow');
-    $("#splash_logo").delay(4000).fadeOut('slow'); 
-    stroke.play();
-  });
-  
-  const webStorage = function () {
-    if (sessionStorage.getItem('visit')) {
-      $("#splash").css("display", "none");
-      $("#splash_logo").css("display", "none");
-      $("#mask").css("display", "none");
-    } else {
-      sessionStorage.setItem('visit', 'true'); 
-      // $("#splash").delay(4000).fadeOut(1800);
-    }
-  }
-  webStorage();
-  
-  
-  
 $(function () {
 
   $('a[href^="#"]').click(function () {
@@ -75,8 +9,6 @@ $(function () {
     return false; 
   });
 });
-
-
 
 
 $('.faq-title').on('click', function () {
